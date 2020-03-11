6e179dd1867f9258ecf36ad70d47c98f1892892b
hogdriver99 <babbittandrew@ymail.com>
2020-03-11 16:22:41 -0400

    basic question template

---
+++ questionbank.js
@@ -0,0 +1,39 @@
+var questionbank = [];
+var rawquestions = ["It is important that society follows a religiously based morality",
+                "We have free will",
+                "Morality is objective",
+                "Following a moral code is important to me",
+                "Technological progress is generally good for society"];
+
+var hasProblemList = [0, 1, 4];
+
+var hasFollowUpList = [2, 3];
+
+var issueFixes = [[
+    ["I disagree with the use of the word society", "I would rather it say humanity", "I would rather it say my country",
+        "I would rather it say the world", "I would rather it say something else", "I don't know what I would replace it with"],
+    ["Society should follow a non-religiously based morality"],
+    ["Society should not be bound by a moral code"]
+]];
+
+var followUps = []
+
+for (let i = 0; i < rawquestions.length; i++) {
+    questionbank[i] = {
+        question: rawquestions[i],
+        hasIssue: false,
+        hasFollowUp: false,
+        issuefixes: [],
+        followUpAddr: -1
+    };
+    if (hasProblemList.includes(i)) {
+        questionbank[i].hasIssue = true;
+        var temp = issueFixes[hasProblemList.indexOf(i)];
+        for (item in temp) {
+            questionbank[i].issuefixes.push(temp[item]);
+        }
+    }
+    if (hasFollowUpList.includes(i)) {
+        questionbank[i].followUpAddr = hasFollowUpList.indexOf(i);
+    }
+}
